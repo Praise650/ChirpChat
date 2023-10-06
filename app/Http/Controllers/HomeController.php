f47274e422e00883c8fd5dfea5bea03509f77374
Praise Afuwape <ayokanmi1275@gmail.com>
2023-10-06 06:38:27 +0100

    Initial commit, I created  the home screen and a model to manage interaction of the page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreHomeRequest;
+use App\Http\Requests\UpdateHomeRequest;
+use App\Models\Home;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = [
+            [
+                'name'=> "Praise Afuwape",
+                'age'=> 22,
+            ],
+            [
+                'name'=> "Sanmi Afuwape",
+                'age'=> 17,
+            ],
+        ];
+        return view(
+            'home',
+            [
+                'users' =>$users
+            ],
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHomeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Home $home)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Home $home)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHomeRequest $request, Home $home)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Home $home)
+    {
+        //
+    }
+}
