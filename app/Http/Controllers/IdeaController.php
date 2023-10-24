34a683095caed54609999c77d823f72d83928996
Praise Afuwape <ayokanmi1275@gmail.com>
2023-10-24 01:52:45 +0100

    implemented create new idea

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -8,11 +8,11 @@ use App\Models\Idea;
 class IdeaController extends Controller
 {
     public function store(){
-        $ideas = new Idea();
+        $idea = new Idea();
         $idea->post = request()->get('idea','');
         // $ideas->post = "Sorry love, Hope you didn't get injured";
         // $ideas->likes = 1;
-        // $ideas->save();
+        $idea->save();
         // dump(Idea::all());
         return redirect()->route('dashboard');
     }
